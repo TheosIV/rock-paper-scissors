57d89fdaba29de1a8a502adf56d7b7fcb967f622
Ilyas Hajib <91851958+TheosIV@users.noreply.github.com>
2023-01-04 14:01:41 +0100

    Create UI

--- script.js
+++ script.js
@@ -1,80 +1,92 @@
-//getComputerChoice function works as expected.
-function getComputerChoice() {
-   let random123 = Math.floor(Math.random()*3+1);
-   if (random123 === 1) {
-    return "Rock";
-   } else if (random123 === 2) {
-    return "Paper";
-   } else {
-    return "Scissors";
-   }
-}
+function computerSelection() {
+    const random123 = Math.floor(Math.random() * 3 + 1);
+    
+    if (random123 === 1) {
+        return "Rock";
+    }else if (random123 === 2) {
+        return "Paper";
+    }else {
+        return "Scissors";
+    };
+};
 
-//getPlayerChoice function works as expected.
-function getPlayerChoice(string) {
-    let strLower = string.toLowerCase();
-    return strLower.charAt(0).toUpperCase() + strLower.slice(1);
-}
-
-//playRound function works as expected.
-function playRound(computerSelection, playerSelection) {
-    if (computerSelection === playerSelection) {
+function playRound(e) {
+    announcement = document.querySelector('#announcement');
+    playerScore = document.querySelector('#player-score');
+    computerScore = document.querySelector('#computer-score');
+    computer = computerSelection();
+    player = e.target.classList.value;
+    player = player.slice(8);
+    if (computer === player) {
+        announcement.textContent = "Draw!";
+        playerScore.textContent = `${++playerScoreCal}`;
+        computerScore.textContent = `${++computerScoreCal}`;
         return 2;
-    } else if (computerSelection === "Rock" && playerSelection === "Paper") {
+    }else if (computer === "Rock" && player === "Paper") {
+        announcement.textContent = "You win!";
+        playerScore.textContent = `${++playerScoreCal}`;
         return 1;
-    } else if (computerSelection === "Paper" && playerSelection === "Scissors") {
+    }else if (computer === "Paper" && player === "Scissors") {
+        announcement.textContent = "You win!";
+        playerScore.textContent = `${++playerScoreCal}`;
         return 1;
-    } else if (computerSelection === "Scissors" && playerSelection === "Rock") {
+    }else if (computer === "Scissors" && player === "Rock") {
+        announcement.textContent = "You win!";
+        playerScore.textContent = `${++playerScoreCal}`;
         return 1;
-    } else if (computerSelection === "Paper" && playerSelection === "Rock") {
+    }else if (computer === "Paper" && player === "Rock") {
+        announcement.textContent = "You Lose!";
+        computerScore.textContent = `${++computerScoreCal}`;
         return 0;
-    } else if (computerSelection === "Scissors" && playerSelection === "Paper") {
+    }else if (computer === "Scissors" && player === "Paper") {
+        announcement.textContent = "You Lose!";
+        computerScore.textContent = `${++computerScoreCal}`;
         return 0;
-    } else if (computerSelection === "Rock" && playerSelection === "Scissors"){
+    }else if (computer === "Rock" && player === "Scissors"){
+        announcement.textContent = "You Lose!";
+        computerScore.textContent = `${++computerScoreCal}`;
         return 0;
-    } else {
+    }else {
         console.log("Please choose between Rock, Paper, or scissors.");
         return -1;
-    }
-}
+    };
+};
 
-//game function works as expected.
-function game() {
-    let playerInput;
-    let playerSelection;
-    let computerSelection;
-    let computerScore = 0;
-    let playerScore = 0;
+function game(e) {
+    const maxScore = Math.max(playerScoreCal, computerScoreCal);
+        const winner = document.querySelector('#winner');
+        playRound(e);
+        if (playerScoreCal === 5 || computerScoreCal === 5) {
+            gameOver.style.zIndex = "999";
+            if (playerScoreCal > computerScoreCal) {
+                winner.textContent = "YOU WIN! CONGRATULATION!!"
+            }else if (playerScoreCal === computerScoreCal) {
+                winner.textContent = "DRAW! BEGINNERS LUCK..."
+            }else {
+                winner.textContent = "YOU LOST! BETTER LUCK NEXT TIME..."
+            };
+            playerScoreCal = 0;
+            computerScoreCal = 0;
+        };
+};
 
-    for (let i = 0; i < 5; i++) {
-        playerInput = prompt("Type your rock, paper, or scissors: ");
-        playerSelection = getPlayerChoice(playerInput);
-        computerSelection = getComputerChoice();
-        const round = playRound(computerSelection, playerSelection);
-        if (round === 1) {
-            ++playerScore;
-            console.log("You win the round!");
-        }else if (round === 0) {
-            ++computerScore;
-            console.log("You lose the round!");
-        }else if (round === 2) {
-            ++playerScore;
-            ++computerScore;
-            console.log("It's a tie, this round!");
-        }else {
-            i--;
-        }
-    }
+let announcement = "";
+let playerScore = "";
+let computerScore = "";
+let computer = "";
+let playerScoreCal = 0;
+let computerScoreCal = 0;
+const choices = document.querySelectorAll('.clicker');
+const tryAgain = document.querySelector('#try-again');
+const gameOver = document.querySelector('#gameover-container');
 
-    if (playerScore > computerScore) {
-        console.log("You win the game!");
-    }else if (playerScore === computerScore) {
-        console.log("It's a tie, this game!");
-    }else {
-        console.log("You lose the game!");
-    }
+choices.forEach(choice => {
+    choice.addEventListener('click', game);
+});
 
-    return `Player|${playerScore}:${computerScore}|Computer`;
-}
-
-console.log(game());
+tryAgain.addEventListener('click', (e) => {
+    gameOver.style.zIndex = "-999";
+    computerScore.textContent = "0";
+    playerScore.textContent = "0";
+    announcement.textContent = "Choose Your Weapon.";
+});
